2975aace3610caf742f8b306a1e8482593c97109
Shobhit-geek <shobhit.raj.41@gmail.com>
2026-01-10 09:11:26 +0530

    code pushed

--- Oops/nestedclass.java
+++ Oops/nestedclass.java
@@ -1,3 +1,3 @@
-public class nestedclass {
+// public class nestedclass {
     
-}
+// }
--- Threads/ExecutorServiceEx.java
+++ Threads/ExecutorServiceEx.java
@@ -1,8 +1,16 @@
 package Threads;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorServiceEx {
     
     public static void main(String[] args) {
-        
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+            2, 5, 1, TimeUnit.HOURS, new ArrayBlockingQueue<>(10));
+        for(int i=1; i<10;i++){
+            
+        }
     }
 }
